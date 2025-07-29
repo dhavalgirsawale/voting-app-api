90a1d4350a12d325f66bc3be175092d47fd6a4f6
Dhaval Girsawale <shakaalgaming1@gmail.com>
2025-07-29 18:02:05 +0530

    Update VoteRepository.java

--- src/main/java/com/example/votingapp/repository/VoteRepository.java
+++ src/main/java/com/example/votingapp/repository/VoteRepository.java
@@ -4,6 +4,5 @@ import com.example.votingapp.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
-    // existsByUserUserIdAndSessionId
     boolean existsByUserUserIdAndSessionId(String userId, Long sessionId);
 }
