3d520bb12fdea34a6dd14f3970067a66c695a834
Dhaval Girsawale <shakaalgaming1@gmail.com>
2025-07-29 18:01:39 +0530

    Removed findByUserIdAndPassword

--- src/main/java/com/example/votingapp/repository/UserRepository.java
+++ src/main/java/com/example/votingapp/repository/UserRepository.java
@@ -4,6 +4,4 @@ import com.example.votingapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
-//    User findByUserIdAndPassword(String userId, String password);
-//    User findByUserIdAndPasswordAndIsAdmin(String userId, String password, boolean isAdmin);
 }
